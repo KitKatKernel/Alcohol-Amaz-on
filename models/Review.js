8f44147dcd9214ae68e32e38847da0e26bd74688
KitKatKernel <167586496+KitKatKernel@users.noreply.github.com>
2024-08-04 22:49:22 -0400

    Worked on Reviews.js Model, added references to foreign key for user_id from User model and beverage_id from Beverage model

--- models/Review.js
+++ models/Review.js
@@ -4,4 +4,44 @@ const sequelize = require('../config/connection');
 // Review model definition
 class Review extends Model {}
 
+Review.init(
+  {
+    // Review ID: Primary key auto-incremented
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // Foreign key referencing User model
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    // Foreign key referencing Beverage model
+    beverage_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'beverage',
+        key: 'id',
+      },
+    },
+    // Review text
+    review: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'review',
+  }
+);
+
 module.exports = Review;
