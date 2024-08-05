ca5491a714876206358f2beb6a4fb0f6f4328a2b
KitKatKernel <167586496+KitKatKernel@users.noreply.github.com>
2024-08-05 18:29:18 -0400

    Bervarge.js updated

---
+++ models/Beverage.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Beverage model definition
+class Beverage extends Model {}
+
+Beverage.init(
+  {
+    // Beverage ID: Primary key, auto-incremented
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // Beverage name
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // Beverage description
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    // Foreign key referencing Ingredient model's ingredient ID.
+    ingredient_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'ingredient',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'beverage',
+  }
+);
+
+module.exports = Beverage;
