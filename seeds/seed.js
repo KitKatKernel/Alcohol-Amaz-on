a3703fc3d56981e411abaca3ec49b9f88f9b2270
KitKatKernel <167586496+KitKatKernel@users.noreply.github.com>
2024-08-05 21:47:06 -0400

    Updated seed.js to correctly seed the database with initial data. Updated models/index.js to define associations between User, Review, Beverage, Ingredient, and lists. Also updated controllers/api/index.js to route future API endpoints.

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,23 +1,46 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Review, Beverage, Ingredient, List } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const reviewData = require('./reviewData.json');
+const beverageData = require('./beverageData.json');
+const ingredientData = require('./ingredientData.json');
+const listData = require('./listData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
+  // Seed Users
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
+  // Seed Ingredients
+  const ingredients = await Ingredient.bulkCreate(ingredientData, {
+    returning: true,
+  });
+
+  // Seed Beverages
+  const beverages = await Beverage.bulkCreate(beverageData.map(bev => ({ name: bev.name, description: bev.description })), {
+    returning: true,
+  });
+
+  // Add ingredients to each beverage
+  for (const beverage of beverageData) {
+    const bev = beverages.find(b => b.name === beverage.name);
+    await bev.addIngredients(beverage.ingredient_ids);
   }
+
+  // Seed Lists
+  const lists = await List.bulkCreate(listData, {
+    returning: true,
+  });
+
+  // Seed Reviews
+  const reviews = await Review.bulkCreate(reviewData, {
+    returning: true,
+  });
 
   process.exit(0);
 };
