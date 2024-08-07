ac0ad5aeaa97eaba6a84ab1a377f79693a7f33a9
Kelsey Frahm <k.rae.heider@gmail.com>
2024-08-06 21:06:15 -0500

    adding drop downs

--- public/js/add-beverage.js
+++ public/js/add-beverage.js
@@ -3,6 +3,7 @@ async function newFormHandler(event) {
   const beverage_name = document.querySelector('#beverage_name').value;
   const description = document.querySelector('#description').value;
   const ingredient = document.querySelector('#ingredient_id').value;
+  const Ingredient = require('../../models')
   // ? Send fetch request to add a new dish
   const response = await fetch(`/api/beverage`, {
     method: 'POST',
@@ -26,3 +27,35 @@ async function newFormHandler(event) {
 document
   .querySelector('.new-beverage-form')
   .addEventListener('submit', newFormHandler);
+
+  function getDropDown() {
+
+    fetch(Ingredient.Ingredient, {
+          method: "GET",
+          }
+      )
+      .then(function (response) {
+        return response.json();
+      })
+  
+      .then((data) => {
+          console.log(data);
+          agencyList.push(data.CodeList[0].ValidValue)
+          console.log(agencyList)
+          return(agencyList)
+        })
+  
+        .then(function (data) {
+          
+          for (let i = 0; i < agencyList[0].length; i++) {
+          const agency = agencyList[0][i]
+  
+          const div = document.createElement('option');
+         
+          div.setAttribute("id", agency.ParentCode)
+          div.textContent = agency.Value
+         
+          agencyDrop.appendChild(div)
+          
+        }})
+      }  
